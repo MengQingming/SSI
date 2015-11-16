34d06c3ab1ecb017e2c29128267e45c8fabd50c1
孟庆铭 <myisdeveloper@163.com>
2015-11-16 09:19:33 +0800

    add RestFullUrlPost

--- src/main/java/com/hm/SSI/ws/RestWebService.java
+++ src/main/java/com/hm/SSI/ws/RestWebService.java
@@ -1,6 +1,8 @@
 package com.hm.SSI.ws;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -9,9 +11,11 @@ import com.hm.SSI.ws.model.RespBean;
 @Path("/app")
 public interface RestWebService {
 	
-	@GET
+//	@GET
+	@POST
 	@Produces("application/json")
 	@Path("/user/login")
-	public RespBean login();
+	public RespBean login(@FormParam("username") String username,
+			@FormParam("password") String password);
 	
 }
--- src/main/java/com/hm/SSI/ws/impl/RestWebServiceImpl.java
+++ src/main/java/com/hm/SSI/ws/impl/RestWebServiceImpl.java
@@ -1,15 +1,21 @@
 package com.hm.SSI.ws.impl;
 
+import org.apache.log4j.Logger;
+
 import com.hm.SSI.ws.RestWebService;
 import com.hm.SSI.ws.model.RespBean;
 
 public class RestWebServiceImpl implements RestWebService{
 	
+	private static Logger log = Logger.getLogger(RestWebServiceImpl.class);
 	
-	public RespBean login(){
+	public RespBean login(String username, String password){
+		log.info("RestWebServiceImpl login--->Begin");
+		log.info("RestWebServiceImpl login--->username:"+username+", password:"+password);
 		RespBean respBean = new RespBean();
 		respBean.setStatus(200);
-		respBean.setMessage("OK");
+		respBean.setMessage("username:"+username+", password:"+password);
+		log.info("RestWebServiceImpl login--->End");
 		return respBean;
 	}
 }
